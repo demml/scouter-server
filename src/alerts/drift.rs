6d0384d3410f092614b4476b1e27bf10ae8a2c5b
Thorrester <sjforrester32@gmail.com>
2024-09-11 11:54:27 -0400

    revert back for scouter

--- src/alerts/drift.rs
+++ src/alerts/drift.rs
@@ -140,12 +140,9 @@ impl DriftExecutor {
 
         // Get alerts
         // keys are the feature names that match the order of the drift array columns
-        let alerts = generate_alerts(
-            &drift_array.view(),
-            keys,
-            &drift_profile.config.alert_config.alert_rule,
-        )
-        .with_context(|| "error generating drift alerts")?;
+        let alert_rule = drift_profile.config.alert_config.alert_rule.clone();
+        let alerts = generate_alerts(&drift_array.view(), keys, alert_rule)
+            .with_context(|| "error generating drift alerts")?;
 
         // Get dispatcher, will default to console if env vars are not found for 3rd party service
         // TODO: Add ability to pass hashmap of kwargs to dispatcher (from drift profile)
